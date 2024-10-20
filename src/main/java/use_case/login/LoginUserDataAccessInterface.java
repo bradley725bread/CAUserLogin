528bceed41525ceea2db61ad526ca9ea9ca6bc42
bradley725bread <bradley725lin@gmail.com>
2024-10-20 14:42:28 -0400

    Task 2.1 complete

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name of the user.
+     */
+    void setCurrentUser(String name);
 }
